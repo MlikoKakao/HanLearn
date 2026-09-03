6cbeed64134e1267a97292e216bf59c9470c6d1a
MlikoKakao <mattewczlp@seznam.cz>
2026-09-03 16:51:21 +0800

    wip: adding zhuyin and building spaced repetition

---
+++ src/HanLearn.Web.Client/Data/StudyCard.cs
@@ -0,0 +1,21 @@
+namespace HanLearn.Web.Client.Data;
+
+public record CardFront
+{
+	public required string Character { get; init; }
+}
+
+public record CardBack
+{
+	public required string Pronunciation { get; init; }
+	public string? Example { get; init; }
+	public string? Voice { get; init; }
+}
+
+
+public record StudyCard
+{
+	public required int Id { get; init; }
+	public required CardFront CardFront { get; init; }
+	public required CardBack CardBack { get; init; }
+}
